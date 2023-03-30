fd2186ec24d8db9cc94b44eaa3d4020f1d80fc74
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-03-29 20:34:36 -0400

    update Region elements

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   to: 'fallback#index',
   constraints: ->(req) { !req.xhr? && req.format.html? }
   
-  resources :users, except: [:new, :edit]
+  resources :users, only: [:create, :show]
   resources :places, except: [:new, :edit]
+  resources :regions, except: [:new, :edit]
   
 
 end

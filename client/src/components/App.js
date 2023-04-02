15545415d0860a31b699b2d9be4dfc9987756f67
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-02 16:03:26 -0400

    move App.js location

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Test from "./pages/Test";
+import React from "react";
+// import Counter from "./features/Counter"
+
+function App() {
+
+  return (
+    <h1>Home</h1>
+    // <BrowserRouter>
+    //   <div className="App">
+    //     <Routes>
+    //       <Route path="/testing" element={<Counter />}/>
+    //       <Route path="/" element={<Home />} />
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
+  );
+}
+
+export default App;

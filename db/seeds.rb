401c1532bcecdbc9221d53ace34d6248be414cda
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 16:42:58 -0400

    added all possible activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 uA = User.create!(email: 'a@gmail.com', first_name: 'A', last_name: 'A', password_digest: 'test')
 uB = User.create!(email: 'b@gmail.com', first_name: 'B', last_name: 'B', password_digest: 'test')
 
@@ -15,6 +7,10 @@ rC = Region.create!(city: 'London', state: 'United Kingdom')
 
 aA = Activity.create!(activity_type: 'Restaurants')
 aB = Activity.create!(activity_type: 'Shopping')
+aC = Activity.create!(activity_type: 'Cafes / Bites')
+aD = Activity.create!(activity_type: 'Site Seeing')
+aE = Activity.create!(activity_type: 'Entertainment / Arts')
+aF = Activity.create!(activity_type: 'Outdoor Recreation')
 
 pA = Place.create!(name: 'Arabesque', map_url: 'https://goo.gl/maps/zRkfdvTFnTpPfXc47', website_url: 'http://www.arabesqueboulder.com/index.php', notes: 'Amazing Middle Eastern food. Must order a chai and the bak lava!', user_id: uA.id, region_id: rA.id, activity_id: aA.id)
 pB = Place.create!(name: 'Absolute Noodle', map_url: 'https://goo.gl/maps/nPB8vE1GbXKBgDb46', website_url: 'https://www.absolutenoodlewashington.com/', notes: 'A great local sushi place. Great sushi with make-your-own soup options.', user_id: uA.id, region_id: rB.id, activity_id: aA.id)

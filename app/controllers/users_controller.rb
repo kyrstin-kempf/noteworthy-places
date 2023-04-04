58e9462b5440091e85ee495b9404766a115a686b
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 16:46:05 -0400

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,31 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
-
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
+  skip_before_action :authorize, only: [:create, :index, :destroy]
+  
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
   end
-
-  # GET /users/:id
+  
   def show
     render json: @user
   end
 
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+  def index 
+    users = User.all 
+    render json: users
   end
 
-  # PATCH/PUT /users/:id
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/:id
-  def destroy
-    @user.destroy
+  def destroy 
+    user = User.find(params[:id])
+    user.destroy
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+    
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :password_digest)
-    end
 end

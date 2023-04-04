b43a9d27022ee52e7c98195f4a2a4218fa28ff3f
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 02:12:11 -0400

    created login page

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import logo from '../assets/Logo.png'
+
+function Login({ onLogin }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    // const [errors, setErrors] = useState([]);
+    // const [isLoading, setIsLoading] = useState(false);
+  
+    // function handleSubmit(e) {
+    //   e.preventDefault();
+    //   setIsLoading(true);
+    //   fetch("/login", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ email, password }),
+    //   })
+    //     .then((r) => {
+    //       setIsLoading(false);
+    //       if (r.ok) {
+    //         r.json().then((user) => onLogin(user));
+    //       } else {
+    //         r.json().then((err) => setErrors(err.errors));
+    //       }
+    //     });
+    // }
+  
+    return (
+      <form className='login-form'>
+        <img className='logo-landing' src={logo} alt='Noteworthy Places logo'/>
+        <label htmlFor="login-email">Email</label>
+        <input
+          type="email"
+          id="login-email"
+          name={email}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="login-password">Password</label>
+        <input
+          type="password"
+          id="login-password"
+          name={password}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className='submit-button-landing'>
+          {/* {isLoading ? "Loading..." : "Login"} */}
+          Log In
+        </button>
+        {/* <div className='login-error'>
+          {errors?.map((err) => (
+            <p key={err}>{err}</p>
+          ))}
+        </div> */}
+      </form>
+    );
+  }
+
+export default Login;

354be08cf949f2d0fbcaf376d0f8070c98e8a090
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 16:44:54 -0400

    add logout handler

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,12 +2,23 @@ import React from "react";
 import { NavLink } from 'react-router-dom';
 import Logo from '../assets/Logo.png';
 
-function NavBar() {
+function NavBar( {setUser} ) {
+
+    function handleLogout() {
+        fetch('/logout', {
+            method: 'DELETE',
+        }).then((r) => {
+            if (r.ok) {
+                setUser(null);
+            }
+        });
+    }
+
     return (
         <div className="nav-bar">
             <NavLink to='/' id="center-logo"><img src={Logo} alt='Noteworthy Places Logo'/></NavLink>
             <NavLink to='/places/new' id="new-place-button">+ New Place</NavLink>
-            <button id="nav-logout">Log Out</button>
+            <button onClick={handleLogout} id="nav-logout">Log Out</button>
         </div>
     )
 }

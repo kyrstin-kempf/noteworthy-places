2d3d6d6180861b0524c41e5d2914caa740e919b2
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 02:13:21 -0400

    added login to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
 import NavBar from "./NavBar";
 import Home from "../pages/Home";
 import OnePlace from "../pages/OnePlace";
@@ -7,8 +8,9 @@ import UpdatePlace from "../pages/UpdatePlace";
 import NewPlace from "../pages/NewPlace";
 
 function App() {
+  const [user, setUser] = useState(null);
 
-// if (!user) return <Login onLogin={setUser} />;
+if (!user) return <Login onLogin={setUser} />;
 
   return (
     <BrowserRouter>

2bfb13e9097fb3bb95c884a5654f9330633697d4
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 16:45:32 -0400

    added macro and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ApplicationRecord
     has_many :places
     has_many :regions, through: :places
     has_many :activities, through: :places
+
+    has_secure_password
+
+    before_save :downcase_email
+
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    
+    private 
+
+    def downcase_email
+        self.email = email.downcase
+    end
+
 end

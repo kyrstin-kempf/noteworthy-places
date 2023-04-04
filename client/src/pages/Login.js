a3c62dcb6ea5baab127f1ff5d9b423f4e3ba4f32
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 02:12:35 -0400

    added login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import LoginForm from '../components/LoginForm';
+import SignUpForm from '../components/SignUpForm';
+
+function Login({ onLogin }) {
+    const [showLogin, setShowLogin] = useState(true);
+
+    return (
+        <div>
+            {showLogin ? (
+            <>
+                <LoginForm onLogin={onLogin} />
+                <hr className='line'></hr>
+                <p className='switch-login'>
+                Don't have an account? &nbsp;
+                <button onClick={() => setShowLogin(false)} className="submit-button"> 
+                Sign Up
+                </button>
+                </p>
+            </>
+            ) : (
+            <>
+                <SignUpForm onLogin={onLogin} />
+                <hr className='line'></hr>
+                <p className='switch-login'>
+                Already have an account? &nbsp;
+                <button onClick={() => setShowLogin(true)} className="submit-button">
+                Log In
+                </button>
+                </p>
+            </>
+            )}
+        </div>
+    );
+
+}
+
+export default Login;

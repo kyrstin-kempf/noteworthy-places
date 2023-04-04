950e434ecf6a761205b5545f0ab7c57dffbb1745
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-04 16:47:11 -0400

    add log in handler

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -4,35 +4,35 @@ import logo from '../assets/Logo.png'
 function Login({ onLogin }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    // const [errors, setErrors] = useState([]);
-    // const [isLoading, setIsLoading] = useState(false);
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
   
-    // function handleSubmit(e) {
-    //   e.preventDefault();
-    //   setIsLoading(true);
-    //   fetch("/login", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ email, password }),
-    //   })
-    //     .then((r) => {
-    //       setIsLoading(false);
-    //       if (r.ok) {
-    //         r.json().then((user) => onLogin(user));
-    //       } else {
-    //         r.json().then((err) => setErrors(err.errors));
-    //       }
-    //     });
-    // }
+    function handleSubmit(e) {
+      e.preventDefault();
+      setIsLoading(true);
+      fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      })
+        .then((r) => {
+          setIsLoading(false);
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors(err.errors));
+          }
+        });
+    }
   
     return (
-      <form className='login-form'>
+      <form className='login-form' onSubmit={handleSubmit}>
         <img className='logo-landing' src={logo} alt='Noteworthy Places logo'/>
         <label htmlFor="login-email">Email</label>
         <input
-          type="email"
+          type="text"
           id="login-email"
           name={email}
           value={email}
@@ -47,14 +47,13 @@ function Login({ onLogin }) {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit" className='submit-button-landing'>
-          {/* {isLoading ? "Loading..." : "Login"} */}
-          Log In
+          {isLoading ? "Loading..." : "Log In"}
         </button>
-        {/* <div className='login-error'>
+        <div className='login-error'>
           {errors?.map((err) => (
             <p key={err}>{err}</p>
           ))}
-        </div> */}
+        </div>
       </form>
     );
   }

e466cbd0f017c37d3179cb3742ed9d47afd8e018
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-05 16:45:40 -0400

    temporarily expanded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,26 @@
 Rails.application.routes.draw do
-  get '*path',
-  to: 'fallback#index',
-  constraints: ->(req) { !req.xhr? && req.format.html? }
   
-  resources :users, only: [:create, :show]
-  resources :places, except: [:new, :edit]
-  resources :regions, except: [:new, :edit]
-  resources :activities, only: [:create]
+  # resources :users, only: [:create, :show]
+  # resources :sessions, only: [:create, :destroy]
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  delete '/users/:id', to: 'users#destroy'
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # resources :places, except: [:new, :edit]
+
+  get '/places', to: 'places#index'
+  post '/places', to: 'places#create'
+
+  # resources :regions, except: [:new, :edit]
+
+  get '/regions', to: 'regions#index'
+
+  # resources :activities, only: [:create]
+
+  get '/activities', to: 'activities#index'
   
 end

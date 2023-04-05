de3a5423f053f98561fee17a9f7bda1a28d11025
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-05 16:44:58 -0400

    redux setup with toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,16 @@
 // External Imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-// Local Imports
 import App from './App';
-import rootReducer from './reducers';
-
-// Assets
+import store from './redux/store';
+import { Provider } from 'react-redux';
 import './App.css';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-  );
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-        <Provider store={store}>
-                <App />
-        </Provider>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );

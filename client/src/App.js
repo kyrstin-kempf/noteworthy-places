968733f6cffb2667a28b2b279d94a2cd001f580e
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-05 16:40:54 -0400

    persist login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./components/Login";
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
-import OnePlace from "./pages/OnePlace";
-import UpdatePlace from "./pages/UpdatePlace";
-import NewPlace from "./pages/NewPlace";
+import Home from "./components/Home";
+import OnePlace from "./components/OnePlace";
+import UpdatePlace from "./components/UpdatePlace";
+import NewPlace from "./components/NewPlace";
+
+// import Login from "./components/Login";
 
 function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    document.title = 'Noteworthy Places';
+  }, []);
+
+  useEffect(() => {
+    fetch('/me').then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
 
 if (!user) return <Login onLogin={setUser} />;
 

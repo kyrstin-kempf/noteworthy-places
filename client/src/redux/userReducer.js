034a64ef201dcb3d9412e1c23322d8ddbb8f09d5
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-08 17:14:21 -0400

    syntax correction

--- client/src/redux/userReducer.js
+++ client/src/redux/userReducer.js
@@ -1,11 +1,3 @@
-import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT,
-  } from "../actions/loginActions";
-
 const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = user
@@ -56,15 +48,15 @@ function userReducer(state = initialState, action) {
     const { type, payload } = action; 
 
     switch (type) {        
-        case REGISTER_SUCCESS:
+        case "REGISTER_SUCCESS":
             return { ...state, isLoggedIn: false }
-        case REGISTER_FAIL:
+        case "REGISTER_FAIL":
             return { ...state, isLoggedIn: false };
-        case LOGIN_SUCCESS:
+        case "LOGIN_SUCCESS":
             return { ...state, isLoggedIn: true, user: payload.user };
-        case LOGIN_FAIL:
+        case "LOGIN_FAIL":
             return { ...state, isLoggedIn: false, user: null };
-        case LOGOUT:
+        case "LOGOUT":
             return { ...state, isLoggedIn: false, user: null };
         default:
             return state;

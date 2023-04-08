68ce8b92b357b8259fb122252f5b1da4ffc3540e
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-08 17:15:23 -0400

    added redux fetch

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,29 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import SearchBox from "./SearchBox";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUser, userSelector } from "../redux/userSlice";
-import Region from './Region.js'
+import PlacesList from "./PlacesList";
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const { user, loading, hasErrors } = useSelector(userSelector)
-
-  useEffect(() => {
-    dispatch(fetchUser());
-  }, [dispatch]);
-
-  const renderUserRegions = () => {
-    if (loading) return <p>Loading...</p>
-    if (hasErrors) return <p>Unable to display regions.</p>
-    
-    return <Region user={user} />
-  }
-
+ 
   return (
     <div>
       <div id="my-places-box">
         <h2 className="page-title">My Places</h2>
-        {renderUserRegions()}
+        <PlacesList />
         <div>
           <SearchBox />
         </div>

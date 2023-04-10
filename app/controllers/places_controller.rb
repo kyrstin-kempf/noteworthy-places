82a6ff345dd3b4a8495f814837bd43d88b4947a1
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-10 15:59:25 -0400

    added places show method

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,31 @@
 class PlacesController < ApplicationController
-  skip_before_action :authorize, only: [:index, :create, :destroy, :update]
+  skip_before_action :authorize, only: [:index, :create, :destroy, :update, :show]
 
   def index
+    # render json: Place.all
     user = User.find_by(id: session[:user_id])
-    # preload region
     places = user.places.includes(:region).order(name: :asc) 
     render json: group_places(places)
+  end
+
+  def show
+    place = Place.find_by(id: params[:id])
+    if place 
+      render json: place
+    else
+      render json: { error: 'Not found' }, status: :not_found 
+    end
+    # user = User.find_by(id: session[:user_id])
+    # # preload region
+    # places = user.places.includes(:region).order(name: :asc) 
+    # placesList = group_places(places)
+    # region = placesList.find_by(region_id: params[:region_id])
+    # place = region.places.find_by(id: params[:id])
+    # if region 
+    #   render json: region 
+    # else
+    #   render json: { error: 'Not found' }, status: :not_found
+    # end
   end
 
   def create 
@@ -39,7 +59,7 @@ class PlacesController < ApplicationController
   def group_places(places)
     groups = places.each_with_object({}) do |place, hsh|
       key = "#{place.region.city}, #{place.region.state}"
-      hsh[key] = { city: place.region.city, state: place.region.state, places: [] } if !hsh.key?(key)
+      hsh[key] = { id: place.region.id, city: place.region.city, state: place.region.state, places: [] } if !hsh.key?(key)
       hsh[key][:places] << place
     end
     groups.values

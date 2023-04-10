d99842d7044e8d5478568511160a9160868fb5ee
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-10 16:05:54 -0400

    serializer cleanup

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,5 +1,3 @@
 class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :activity_type
-
-  has_many :places
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :first_name, :last_name
-
-  # def user_regionplaces 
-  #   adjusted_regions = object.regions.map do |region|
-  #     ar = {}
-  #     ar[:city] = region.city
-  #     ar[:state] = region.state
-  #     ar[:places] = region.places.order("name").select { |p| p.user_id == object.id }
-  #     ar
-  #   end
-  #   adjusted_regions
-  # end
-
 end

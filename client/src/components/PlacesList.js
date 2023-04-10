275777b42e3f737c2ee5116b6a25c29d32c912e4
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-10 16:01:02 -0400

    added error handler

--- client/src/components/PlacesList.js
+++ client/src/components/PlacesList.js
@@ -3,28 +3,30 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 
 export const PlacesList = () => {
-  const regions = useSelector(state => state.data.regions)
-
-  const regionsAsc = [...regions].sort((a, b) => a.city > b.city ? 1 : -1);
-  // console.log(citiesDescending);
-
+  const regions = useSelector(state => state.places.places)
+  // console.log(regions)
+  
+  // const regionsAsc = [...regions].sort((a, b) => a.city > b.city ? 1 : -1)
+  
   return (
     <div>
-      {regionsAsc.map(r => {
-        return <section key={r.city}>
+      {regions.length > 0 ? (
+        [...regions].sort((a, b) => a.city > b.city ? 1 : -1).map(r => {
+        return <section key={r.id}>
               <h3>{r.city}, {r.state}</h3>
               {r.places.map( p => 
                 <ul key={p.id}>
-                    <li>{p.name} 
-                    <Link to={`/places/${p.id}`}>
-                      <button id='details-button'>Details</button>
-                    </Link>
+                    <li>{p.name}
+                    <Link to={`places/${p.id}`}>
+                      <button type='button' id='details-button'>Details</button>
+                    </Link> 
                     </li>
                 </ul>)
                 }
             </section>
-        })
-      }
+        }) ) : (
+          <p>No Places Found</p>
+        )}
     </div>
   )
 }

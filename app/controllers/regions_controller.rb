2f9409674703600b52722f9a498241cd2becc742
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-10 16:04:05 -0400

    added create method

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -2,8 +2,18 @@ class RegionsController < ApplicationController
   skip_before_action :authorize, only: [:index]
 
   def index
-    regions = Region.all
+    user = User.find_by(id: session[:user_id])
+    regions = user.regions
     render json: regions
   end
 
+  def create
+    render json: Region.create!(regions_params)
+  end
+
+  private
+
+  def regions_params
+    params.require(:region).permit(:state, :city)
+  end
 end

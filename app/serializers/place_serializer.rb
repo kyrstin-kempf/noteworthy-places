c4723a0571602a55b21d45321c55c9415c8a41e9
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-10 16:03:15 -0400

    cleaned up serializer

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,6 +1,3 @@
 class PlaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :map_url, :website_url, :notes
-
-  # belongs_to :region
-
+  attributes :id, :name, :map_url, :website_url, :notes, :region_id
 end

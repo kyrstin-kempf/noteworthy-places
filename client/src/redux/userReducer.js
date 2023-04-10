4bcc27e79473b66a7e4a9bdf051e69813ae47224
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-10 15:59:09 -0400

    set userReducer

--- client/src/redux/userReducer.js
+++ client/src/redux/userReducer.js
@@ -1,62 +1,30 @@
-const user = JSON.parse(localStorage.getItem("user"));
-
-const initialState = user
-  ? { isLoggedIn: true, user }
-  : { isLoggedIn: false, user: null };
-
+const initialState = { isLoggedIn: false, user: null, loading: true };
 
 export function fetchUser() {
     return function (dispatch) {
       dispatch({ type: "user/userLoading" });
       fetch("/me")
-        .then((response) => response.json())
-        .then((user) =>   {
-            console.log('user', user)
-            dispatch({
-                type: "user/userLoaded",
-                payload: user,
-            })
-            }
-        );
+        .then((r) => {
+            if (r.ok) {
+                r.json().then((user) => {                 
+                  dispatch({ type: "user/userLoaded", payload: user })
+                });
+              } else {
+                dispatch({ type: "user/userLoaded", payload: null })
+              }
+        })
     };
 }
-
-// export function loginUser(e) {
-//     e.preventDefault();
-//     dispatch({ type: "user/loginLoading" })
-//     // setIsLoading(true);
-//     fetch("/login", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ email, password }),
-//     })
-//       .then((r) => {
-//         // setIsLoading(false);
-//         // dispatch({ type: "user/loggedIn" })
-//         if (r.ok) {
-//         //   r.json().then((user) => onLogin(user));
-//           r.json().then((user) => dispatch({ type: 'user/loggedIn', payload: user}));
-//         } else {
-//           r.json().then((err) => setErrors(err.errors));
-//         }
-//       });
-//   }
 
 function userReducer(state = initialState, action) {
     const { type, payload } = action; 
 
     switch (type) {        
-        case "REGISTER_SUCCESS":
-            return { ...state, isLoggedIn: false }
-        case "REGISTER_FAIL":
-            return { ...state, isLoggedIn: false };
-        case "LOGIN_SUCCESS":
-            return { ...state, isLoggedIn: true, user: payload.user };
-        case "LOGIN_FAIL":
-            return { ...state, isLoggedIn: false, user: null };
-        case "LOGOUT":
+        case "user/userLoading":
+            return { ...state, isLoggedIn: false, loading: true }
+        case "user/userLoaded":
+            return { ...state, isLoggedIn: !!payload, user: payload, loading: false };
+        case "user/userLoggedOut":
             return { ...state, isLoggedIn: false, user: null };
         default:
             return state;

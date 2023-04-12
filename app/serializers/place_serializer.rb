4dc5643bca6de91889c2c410158b01a5ad5e3db7
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-11 23:15:11 -0400

    added userId attr

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,3 +1,3 @@
 class PlaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :map_url, :website_url, :notes, :region_id, :activity_id
+  attributes :id, :name, :map_url, :website_url, :notes, :region_id, :activity_id, :user_id
 end

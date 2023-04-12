3c79160e14a508f441fdf80d04e9b9a02243b966
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-11 20:41:32 -0400

    added create method

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,4 +6,14 @@ class ActivitiesController < ApplicationController
     render json: activities
   end
 
+  def create
+    render json: Activity.create!(activity_params)
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:activity_type)
+  end
+
 end

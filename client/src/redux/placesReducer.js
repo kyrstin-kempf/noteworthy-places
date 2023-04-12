9635913e408426408c20a5a4162dbabcdbc29ccb
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-12 03:41:52 -0400

    added filter reducers

--- client/src/redux/placesReducer.js
+++ client/src/redux/placesReducer.js
@@ -1,6 +1,5 @@
-// create and read activities
-
 const initialState = {
+    reservedPlaces: [],
     places: [],
     loading: false,
 }
@@ -25,9 +24,9 @@ function placesReducer(state = initialState, action) {
         case "places/placesLoading":
             return { ...state, loading: true }
         case "places/placesLoaded":
-            return { places: action.payload, loading: false };
+            return { reservedPlaces: action.payload, places: action.payload, loading: false };
         case "places/addNewPlace":
-            return { ...state, places: [...state.places, action.payload], loading: false };
+            return { ...state, reservedPlaces: [...state.reservedPlaces, action.payload], places: [...state.places, action.payload], loading: false };
         case "places/updatePlace":
             let placeId = action.payload.id
             let newPlaces = state.places.map(p => {
@@ -35,17 +34,15 @@ function placesReducer(state = initialState, action) {
                     return action.payload
                 } return p
             })
-            return {...state, places: newPlaces}
+            return {...state, reservedPlaces: newPlaces, places: newPlaces }
         case "places/deletePlace":
-            return { ...state, places: state.places.filter(p => p.id.toString() !== action.payload ) };
+            return { ...state, places: state.places.filter(p => p.id.toString() !== action.payload), reservedPlaces: state.reservedPlaces.filter(p => p.id.toString() !== action.payload) };
         case "places/filterByActivity":
             let actId = action.payload;
-            let filteredPlaces = state.places.filter(p => {
-                return p.activity_id === (actId)
-            });
-            return { ...state, places: filteredPlaces };
+            let filteredPlaces = state.places.filter(p => p.activity_id.toString() === actId);
+            return { ...state, places: filteredPlaces }
         case "places/clearFilter":
-           return { ...state, places: [...state.places] };
+           return { ...state, places: state.reservedPlaces };
         default:
             return state;
     }

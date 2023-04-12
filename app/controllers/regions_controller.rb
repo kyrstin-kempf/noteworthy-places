359561524080e2b0c3a3e759be4f38a529747720
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-11 20:42:07 -0400

    added update and destroy methods

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,14 +1,24 @@
 class RegionsController < ApplicationController
-  skip_before_action :authorize, only: [:index]
+  skip_before_action :authorize, only: [:index, :destroy, :update]
 
   def index
-    user = User.find_by(id: session[:user_id])
-    regions = user.regions
-    render json: regions
+    render json: Region.all
   end
 
   def create
     render json: Region.create!(regions_params)
+  end
+
+  def update 
+    region = Region.find(params[:id])
+    region.update(regions_params) 
+    render json: region
+  end
+
+  def destroy 
+    region = Region.find(params[:id])
+    region.destroy
+    head :no_content
   end
 
   private

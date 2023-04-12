0591e1f916f252c19e1ba947482e5dfefc1c3b4a
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-11 20:46:11 -0400

    added activities from store

--- client/src/components/PlacesList.js
+++ client/src/components/PlacesList.js
@@ -3,21 +3,32 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 
 export const PlacesList = () => {
-  const regions = useSelector(state => state.places.places)
+  const places = useSelector(state => state.places.places)
+  const regions = useSelector(state => state.regions.regions)
+  const activities = useSelector(state => state.activities.activities)
+  
+  // console.log(places)
   // console.log(regions)
-  
+  // console.log(activities)
+
   // const regionsAsc = [...regions].sort((a, b) => a.city > b.city ? 1 : -1)
-  
+  // console.log(regionsAsc)
+
+  function activityMatch(activityId) {
+    const act = activities.find(a => a.id === activityId)
+    return act.activity_type
+  }
+
   return (
     <div>
       {regions.length > 0 ? (
         [...regions].sort((a, b) => a.city > b.city ? 1 : -1).map(r => {
         return <section key={r.id}>
               <h3>{r.city}, {r.state}</h3>
-              {r.places.map( p => 
+              {places.filter(place => place.region_id === r.id).map(p =>
                 <ul key={p.id}>
-                    <li>{p.name}
-                    <Link to={`places/${p.id}`}>
+                    <li>{p.name} | { activityMatch(p.activity_id) }
+                    <Link to={`/places/${p.id}`}>
                       <button type='button' id='details-button'>Details</button>
                     </Link> 
                     </li>

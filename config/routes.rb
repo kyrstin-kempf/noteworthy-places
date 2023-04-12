f94d5718620003398c32cc6ec3f6a1dc64a81ba3
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-12 05:48:39 -0400

    abstracted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,13 @@
 Rails.application.routes.draw do
-  
-  # resources :users, only: [:create, :show]
-  # resources :sessions, only: [:create, :destroy]
 
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
-  delete '/users/:id', to: 'users#destroy'
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  # resources :places, except: [:new, :edit]
-
-  get '/places', to: 'places#index'
-  post '/places', to: 'places#create'
-  get '/places/:id', to: 'places#show'
-  delete '/places/:id', to: 'places#destroy'
-
-  # resources :regions, except: [:new, :edit]
-
-  get '/regions', to: 'regions#index'
-  post '/regions', to: 'regions#create'
-  delete 'regions/:id', to: 'regions#destroy'
-  patch 'regions/:id', to: 'regions#update'
-
-  # resources :activities, only: [:create]
-
-  get '/activities', to: 'activities#index'
-  post '/activities', to: 'activities#create'
+  resources :places, except: [:new, :edit]
+  resources :regions, only: [:index, :create]
+  resources :activities, only: [:index, :create]
   
 end

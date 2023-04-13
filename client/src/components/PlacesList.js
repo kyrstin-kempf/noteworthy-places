40d21bd9b35567044388d1c031c775285329b2ba
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-13 04:41:43 -0400

    modified order and group display of places

--- client/src/components/PlacesList.js
+++ client/src/components/PlacesList.js
@@ -1,41 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux'
+import bookmark from '../assets/bookmark.png';
 
 export const PlacesList = () => {
   const places = useSelector(state => state.places.places)
   const regions = useSelector(state => state.regions.regions)
   const activities = useSelector(state => state.activities.activities)
-  
-  // console.log(places)
-  // console.log(regions)
-  // console.log(activities)
-
-  // const regionsAsc = [...regions].sort((a, b) => a.city > b.city ? 1 : -1)
-  // console.log(regionsAsc)
 
   function activityMatch(activityId) {
       const act = activities.find(a => a.id === activityId)
-      return act.activity_type
+      return act && act.activity_type
+  }
+
+  function getUserPlaces() {
+      const groupedPlaces =  places.reduce((obj, place) => {
+        obj[place.region_id] ||= [] // region_id 1 { 1: []}
+        obj[place.region_id].push(place)
+        return obj
+      }, {});
+
+    const data = [];
+    [...regions].sort((a, b) => a.city > b.city ? 1 : -1).forEach(r => {
+      const regionPlaces = groupedPlaces[r.id];
+      if (regionPlaces && regionPlaces.length > 0) {
+        data.push(<section key={r.id}>
+          <div className='location-name-box'>
+            <h3 className='location-name'>{r.city}, {r.state}</h3>
+            <span className='location-bookmark'><img src={bookmark} alt='bookmark'/></span>
+          </div>
+          {regionPlaces.map(p =>
+          <div className='row-home' key={p.id}>	
+      
+          <div className='col-30'>
+            <ul>
+              <li>{p.name}</li>
+            </ul>
+          </div>
+          <div className='col-30'>
+            <p>{ activities.length > 0 ? activityMatch(p.activity_id) : 'No activities found' }</p>
+          </div>
+          <div className='col-30'>
+            <Link to={`/places/${p.id}`}>
+            <button type='button' id='details-button'>Details</button>
+            </Link> 
+          </div>
+
+            </div>
+          )}
+        </section>)
+      }
+    })     
+
+    return data
   }
 
   return (
     <div>
-      {regions.length > 0 ? (
-        [...regions].sort((a, b) => a.city > b.city ? 1 : -1).map(r => {
-        return <section key={r.id}>
-              <h3>{r.city}, {r.state}</h3>
-              {places.filter(place => place.region_id === r.id).map(p =>
-                <ul key={p.id}>
-                    <li>{p.name} | { activities.length > 0 ? activityMatch(p.activity_id) : <p>No activities found</p> }
-                    <Link to={`/places/${p.id}`}>
-                      <button type='button' id='details-button'>Details</button>
-                    </Link> 
-                    </li>
-                </ul>)
-                }
-            </section>
-        }) ) : (
+      {regions.length > 0 ? getUserPlaces() : (
           <p>No Places Found</p>
         )}
     </div>

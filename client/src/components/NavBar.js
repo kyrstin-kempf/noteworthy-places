c9249f2e43583b97c1874ed62276b26c9e2eb609
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-13 04:44:22 -0400

    changed logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { NavLink, useNavigate } from 'react-router-dom';
-import Logo from '../assets/Logo.png';
+import newlogo from '../assets/newlogo.png';
 
 function NavBar() {
     const dispatch = useDispatch();
@@ -20,7 +20,7 @@ function NavBar() {
 
     return (
         <div className="nav-bar">
-            <NavLink to='/' id="center-logo"><img src={Logo} alt='Noteworthy Places Logo'/></NavLink>
+            <NavLink to='/' id="center-logo"><img src={newlogo} alt='Noteworthy Places Logo'/></NavLink>
             <NavLink to='/places/new' id="new-place-button">+ New Place</NavLink>
             <button onClick={handleLogout} id="nav-logout">Log Out</button>
         </div>

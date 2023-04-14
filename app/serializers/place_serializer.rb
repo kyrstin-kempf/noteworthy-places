b5e81bcaefef6c39313551b77a62c9c18a0caae9
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-14 03:38:47 -0400

    added assocations

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,3 +1,7 @@
 class PlaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :map_url, :website_url, :notes, :region_id, :activity_id, :user_id
+  attributes :id, :name, :map_url, :website_url, :notes, :region_id
+
+  belongs_to :user
+  belongs_to :region
+  belongs_to :activity
 end

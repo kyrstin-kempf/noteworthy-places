0e98bf73c6305abe31de9d184c3fcdd741415c60
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-04-14 03:46:40 -0400

    clean up

--- client/src/components/PlacesList.js
+++ client/src/components/PlacesList.js
@@ -5,63 +5,48 @@ import bookmark from '../assets/bookmark.png';
 
 export const PlacesList = () => {
   const places = useSelector(state => state.places.places)
-  const regions = useSelector(state => state.regions.regions)
-  const activities = useSelector(state => state.activities.activities)
-
-  function activityMatch(activityId) {
-      const act = activities.find(a => a.id === activityId)
-      return act && act.activity_type
-  }
-
-  function getUserPlaces() {
-      const groupedPlaces =  places.reduce((obj, place) => {
-        obj[place.region_id] ||= [] // region_id 1 { 1: []}
-        obj[place.region_id].push(place)
-        return obj
-      }, {});
-
-    const data = [];
-    [...regions].sort((a, b) => a.city > b.city ? 1 : -1).forEach(r => {
-      const regionPlaces = groupedPlaces[r.id];
-      if (regionPlaces && regionPlaces.length > 0) {
-        data.push(<section key={r.id}>
-          <div className='location-name-box'>
-            <h3 className='location-name'>{r.city}, {r.state}</h3>
-            <span className='location-bookmark'><img src={bookmark} alt='bookmark'/></span>
-          </div>
-          {regionPlaces.map(p =>
-          <div className='row-home' key={p.id}>	
-      
-          <div className='col-30'>
-            <ul>
-              <li>{p.name}</li>
-            </ul>
-          </div>
-          <div className='col-30'>
-            <p>{ activities.length > 0 ? activityMatch(p.activity_id) : 'No activities found' }</p>
-          </div>
-          <div className='col-30'>
-            <Link to={`/places/${p.id}`}>
-            <button type='button' id='details-button'>Details</button>
-            </Link> 
-          </div>
-
-            </div>
-          )}
-        </section>)
-      }
-    })     
-
-    return data
-  }
+  const placesCopy = [...places]
+  const sortedPlaces = places && placesCopy.sort((a, b) => a.region.city > b.region.city ? 1 : -1)  
 
   return (
     <div>
-      {regions.length > 0 ? getUserPlaces() : (
-          <p>No Places Found</p>
-        )}
+      {places.length > 0 ? (
+        sortedPlaces.map((p) => (
+          <section key={p.id}>
+            <div className='location-name-box'>
+              <h3 className='location-name'>{p.region.city}, {p.region.state}</h3>
+              <span className='location-bookmark'><img src={bookmark} alt='bookmark'/></span>
+            </div>
+          
+              <div className='row-home' key={p.id}>	
+
+                <div className='col-30'>
+                  <ul>
+                    <li>{p.name}</li>
+                  </ul>
+                </div>
+
+                <div className='col-30'>
+                  <p>{p.activity.activity_type}</p>
+                </div>
+
+                <div className='col-30'>
+                  <Link to={`/places/${p.id}`}>
+                  <button type='button' id='details-button'>Details</button>
+                  </Link> 
+                </div>
+
+              </div>
+
+          </section>
+        ))
+      ) : (
+        <div>
+          <h2>No Places Found</h2>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default PlacesList;
